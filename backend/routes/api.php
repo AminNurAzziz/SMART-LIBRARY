dd9882ab4383a0116615d58fb673db2d70d9c4cf
007xBramasta <bramasta026x@gmail.com>
2024-04-25 14:55:17 +0700

    add new routes history

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BukuController;
 use App\Http\Controllers\PeminjamanBuku;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\RegulationController;
+use App\Http\Controllers\HistoryPeminjamanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +20,23 @@ use App\Http\Controllers\RegulationController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    // Rute di bawah ini memerlukan autentikasi
+    Route::middleware('auth.jwt')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::get('me', [AuthController::class, 'me']);
+    });
 });
+
+Route::middleware('auth.jwt')->group(function () {
+    Route::get('/allhistory-peminjaman', [HistoryPeminjamanController::class, 'getAllHistory']);
+    Route::delete('/history-peminjaman/{id}', [HistoryPeminjamanController::class, 'deleteHistory']);
+});
+
 
 Route::get('/buku', [BukuController::class, 'getBuku']);
 Route::post('/peminjaman-buku', [PeminjamanBuku::class, 'pinjamBuku']);
@@ -30,3 +46,4 @@ Route::get('/student', [StudentController::class, 'getStudentStatuses']);
 Route::patch('/perpanjangan-buku/{id_detail_pinjamm}', [PeminjamanBuku::class, 'createPerpanjangan']);
 Route::post('/reservasi-buku', [PeminjamanBuku::class, 'reserveBook']);
 Route::patch('/konfirmasi-reservasi/{id_detail_reservasi}', [PeminjamanBuku::class, 'createKonfirmasiReservasi']);
+Route::get('/history-peminjaman', [HistoryPeminjamanController::class, 'getHistoryByNIM']);
