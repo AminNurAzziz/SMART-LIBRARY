4cf6513f67e39074448f6e9a55acc92de0114ff3
007xBramasta <bramasta026x@gmail.com>
2024-04-26 15:02:06 +0700

    add service and student controller

--- backend/app/Http/Controllers/StudentController.php
+++ backend/app/Http/Controllers/StudentController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use App\Http\Services\StudentService;
+use Illuminate\Validation\ValidationException;
 
 class StudentController extends Controller
 {
@@ -14,22 +16,119 @@ class StudentController extends Controller
         $this->studentService = $studentService;
     }
 
-    public function getStudentStatuses(Request $request)
+    public function createStudent(Request $request)
     {
-        $nim = $request->query('nim');
-        if (!$nim) {
-            return response()->json(['message' => 'NIM is required'], 400);
-        }
+        try {
+            $student = $this->studentService->createStudent($request->all());
 
-        $result = $this->studentService->getStudentWithBorrowingData($nim);
-        if (!$result) {
-            return response()->json(['message' => 'Student not found'], 404);
-        }
+            return response()->json([
+                'message' => 'Create Student Successfully',
+                'status' => 'success',
+                'code' => 201,
+                'data' => [
+                    'student' => [
+                        'nim' => $student->nim,
+                        'name' => $student->nama_mhs,
+                        'major' => $student->prodi_mhs,
+                        'class' => $student->kelas_mhs,
+                        'email' => $student->email_mhs,
+                        'status' => $student->status_mhs,
+                    ],
+                ],
+            ], 201);
+        } catch (ValidationException $e) {
+            Log::error("Failed to create student: {$e->getMessage()}");
+            $errorsDetails = [];
+            foreach ($e->errors() as $field => $messages) {
+                foreach ($messages as $message) {
+                    $errorsDetails[] = [
+                        'msg' => $message,
+                        'path' => $field,
+                    ];
+                }
+            }
 
+            return response()->json([
+                'statusCode' => 400,
+                'status' => false,
+                'message' => 'Invalid Request Parameter',
+                'error' => 'Bad Request',
+                'errors' => $errorsDetails,
+            ], 400);
+        } catch (\Exception $e) {
+            Log::error("Failed to create student: {$e->getMessage()}");
+            return response()->json([
+                'statusCode' => 500,
+                'status' => false,
+                'message' => 'Internal Server Error',
+                'error' => 'Internal Server Error',
+            ], 500);
+        }
+    }
+
+    public function updateStudent(Request $request)
+    {
+        try {
+            $student = $this->studentService->updateStudent($request->all());
+
+            return response()->json([
+                'message' => 'Update Student Successfully',
+                'status' => 'success',
+                'code' => 200,
+                'data' => [
+                    'nim' => $student->nim,
+                    'name' => $student->nama_mhs,
+                    'major' => $student->prodi_mhs,
+                    'class' => $student->kelas_mhs,
+                    'email' => $student->email_mhs,
+                    'status' => $student->status_mhs,
+                ],
+            ], 200);
+        } catch (ValidationException $e) {
+            Log::error("Failed to update student: {$e->getMessage()}");
+            $errorsDetails = [];
+            foreach ($e->errors() as $field => $messages) {
+                foreach ($messages as $message) {
+                    $errorsDetails[] = [
+                        'msg' => $message,
+                        'path' => $field,
+                    ];
+                }
+            }
+
+            return response()->json([
+                'statusCode' => 400,
+                'status' => false,
+                'message' => 'Invalid Request Parameter',
+                'error' => 'Bad Request',
+                'errors' => $errorsDetails,
+            ], 400);
+        } catch (\Exception $e) {
+            Log::error("Failed to update student: {$e->getMessage()}");
+            return response()->json([
+                'statusCode' => 500,
+                'status' => false,
+                'message' => 'Internal Server Error',
+                'error' => 'Internal Server Error',
+            ], 500);
+        }
+    }
+
+    public function  deleteStudent($nim)
+    {
+        $student = $this->studentService->deleteStudent($nim);
         return response()->json([
-            'message' => 'Student data retrieved successfully',
-            'student' => $result['student'],
-            'borrowing_data' => $result['borrowing_data']
+            'message' => 'Delete Student Successfully',
+            'status' => 'success',
+            'code' => 200,
+            'data' => [
+                'nim' => $student->nim,
+                'name' => $student->nama_mhs,
+                'major' => $student->prodi_mhs,
+                'class' => $student->kelas_mhs,
+                'email' => $student->email_mhs,
+                'status' => $student->status_mhs,
+            ],
         ], 200);
     }
 }
